34158ad18db5c5e97eb8bdee7293cbe2bdeef8d5
Pengu2012 <tobechi.ogosi@outlook.com>
2024-12-11 16:16:17 +0000

    Add files via upload

--- AlienClickGame.py
+++ AlienClickGame.py
@@ -15,5 +15,17 @@ def draw():
     screen.fill(color =(120,0,0))
     alien.draw()
     screen.draw.text(Message,center=(400,10),fontsize = 30)
+def place_alien():
+    alien.x = randint(50,450)
+    alien.y = randint(50,450)
+def on_mouse_down(pos):
+    global Message
+    if alien.collidepoint(pos):
+        Message = "You hit it!"
+        place_alien()
+    else:
+        Message = "Not even close"
 
+
+place_alien()
 pgzrun.go()
